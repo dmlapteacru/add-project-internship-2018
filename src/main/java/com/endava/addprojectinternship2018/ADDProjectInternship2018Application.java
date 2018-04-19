f51533472b04eed7bef35c0b873e080cff913a98
Dumitru Lapteacru <Dumitru.Lapteacru@endava.com>
2018-04-19 14:36:05 +0300

    Created project.

---
+++ src/main/java/com/endava/addprojectinternship2018/ADDProjectInternship2018Application.java
@@ -0,0 +1,12 @@
+package com.endava.addprojectinternship2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ADDProjectInternship2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ADDProjectInternship2018Application.class, args);
+	}
+}
---
+++ src/main/java/com/endava/addprojectinternship2018/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.endava.addprojectinternship2018;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ADDProjectInternship2018Application.class);
+	}
+
+}
