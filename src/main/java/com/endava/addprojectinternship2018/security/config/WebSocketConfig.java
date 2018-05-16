c641ef02032cbac0a1799e73fb7c87ddf939d296
dmlapteacru <lapteacrudumitru@aol.com>
2018-05-16 03:15:14 +0300

    websocket

---
+++ src/main/java/com/endava/addprojectinternship2018/security/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.endava.addprojectinternship2018.security.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+
+@Deprecated
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/websocket-example")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,53 @@
+var stompClient = null;
+$(document).ready(function () {
+   connect();
+});
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+}
+
+function connect() {
+    var socket = new SockJS('/websocket-example');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        console.log("before CON");
+        stompClient.subscribe('/topic/user', function (greeting) {
+            console.log("CON");
+            showGreeting(JSON.parse(greeting.body).content);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    stompClient.send("/app/user", {}, JSON.stringify({'name': $("#name").val()}));
+}
+
+function showGreeting(message) {
+    $("#userinfo").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+});
