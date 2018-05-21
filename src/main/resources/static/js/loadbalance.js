aa4d2bc8f654bf5a8898507391bb5b097549198d
dmlapteacru <lapteacrudumitru@aol.com>
2018-05-21 14:33:37 +0300

    no connection bank - resolved. app version 0.1.0

--- src/main/resources/static/js/loadbalance.js
+++ src/main/resources/static/js/loadbalance.js
@@ -1,16 +1,16 @@
 $( document ).ready(function () {
-    loadBalance()
+    loadBalance();
+    $(".balance").text(" -no connection- ");
+    $(".balance").css("font-size", "13px");
+    $(".balance_info").text(" -no connection- ");
+    $(".balance_info").css("font-size", "12px");
+    $("#mdl").css("display", "none");
 });
 function loadBalance() {
     $.ajax(
         {
             statusCode: {
               400: function() {
-                  $(".balance").text(" -no connection- ");
-                  $(".balance").css("font-size", "13px");
-                  $(".balance_info").text(" -no connection- ");
-                  $(".balance_info").css("font-size", "12px");
-                  $("#mdl").css("display", "none");
               }
             },
             url : "/bankAccount/balance",
@@ -23,6 +23,7 @@ function loadBalance() {
 }
 
 function parseBalance(string) {
+    $("#mdl").css("display", "block");
     var balance = string.split(":")[1].substring(0, string.split(":")[1].length-1);
     $(".balance").text(balance);
     $(".balance_info").text(balance);
